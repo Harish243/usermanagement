720b9cabc09d3667de48672176edcc4ce1116032
Harish <paruchuriharish@yahoo.com>
2017-10-29 00:35:17 +0530

    login app

---
+++ controllers/api/users.controller.js
@@ -0,0 +1,90 @@
+﻿var config = require('config.json');
+var express = require('express');
+var router = express.Router();
+var userService = require('services/user.service');
+
+// routes
+router.post('/authenticate', authenticateUser);
+router.post('/register', registerUser);
+router.get('/current', getCurrentUser);
+router.put('/:_id', updateUser);
+router.delete('/:_id', deleteUser);
+//custom module
+module.exports = router;
+
+//Authenticating the user
+function authenticateUser(req, res) {
+    userService.authenticate(req.body.username, req.body.password)
+        .then(function (token) {
+            if (token) {
+                // If the authentication successful
+                res.send({ token: token });
+            } else {
+                // If the authentication failed
+                res.status(401).send('Username or password is incorrect');
+            }
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+//User Registeration
+function registerUser(req, res) {
+    userService.create(req.body)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+//Get the current user
+function getCurrentUser(req, res) {
+    userService.getById(req.user.sub)
+        .then(function (user) {
+            if (user) {
+                res.send(user);
+            } else {
+                res.sendStatus(404);
+            }
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+//Updating the user
+function updateUser(req, res) {
+    var userId = req.user.sub;
+    if (req.params._id !== userId) {
+        // can only update own account
+        return res.status(401).send('You can only update your own account');
+    }
+
+    userService.update(userId, req.body)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
+
+//Deleteing the user
+function deleteUser(req, res) {
+    var userId = req.user.sub;
+    if (req.params._id !== userId) {
+        // delete own account
+        return res.status(401).send('You can only delete your own account');
+    }
+
+    userService.delete(userId)
+        .then(function () {
+            res.sendStatus(200);
+        })
+        .catch(function (err) {
+            res.status(400).send(err);
+        });
+}
